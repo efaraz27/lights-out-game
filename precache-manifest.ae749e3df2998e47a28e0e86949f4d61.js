a5d6e4690fb32c324631410dcd3db0f45ffe094e
Ehtesham Faraz <efaraz27@gmail.com>
2021-02-17 16:29:32 +0530

    Updates

---
+++ precache-manifest.ae749e3df2998e47a28e0e86949f4d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fce2ead7d7206e432bb1",
+    "url": "/lights-out-game/static/css/main.0cb8a182.chunk.css"
+  },
+  {
+    "revision": "fce2ead7d7206e432bb1",
+    "url": "/lights-out-game/static/js/main.fce2ead7.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/lights-out-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "6cd7317ea2931a12a709e0650bf97e21",
+    "url": "/lights-out-game/index.html"
+  }
+];
